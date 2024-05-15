11a94fa8fbefee69de813c8842e28ee2032dbb87
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-05-15 16:56:31 -0500

    landing page

---
+++ app.js
@@ -0,0 +1,14 @@
+/* funcinalidad nava */
+function mostrarBarraLateral() {
+    let barraLateral = document.querySelector(".barraLateral")
+    barraLateral.style.display = "flex"
+}
+
+function ocultarBarraLateral() {
+    let barraLateral = document.querySelector(".barraLateral")
+    barraLateral.style.display = "none"
+}
+
+/* carruse */
+
+      
